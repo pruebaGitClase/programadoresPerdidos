aee8b3fa943878745f97caa8aad2139a134264a2
albertoobreo <59649525+albertoobreo@users.noreply.github.com>
2023-02-07 10:10:18 +0100

    inicial develop

---
+++ pruebaGitClase/src/test/java/com/example/pruebagitclase/PruebaGitClaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pruebagitclase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaGitClaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
